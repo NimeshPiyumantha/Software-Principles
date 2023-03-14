e5273ba1f3f2941365dd0336e21073612d22f676
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-15 04:31:54 +0530

    update BankAppWithdrawalService

--- _01_Open-Close-Principle/src/BankAppWithdrawalService.java
+++ _01_Open-Close-Principle/src/BankAppWithdrawalService.java
@@ -1,9 +1,11 @@
+import service.Account;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class BankAppWithdrawalService {
-    public void depositToSavingsAccount(double amount){
+   /* public void depositToSavingsAccount(double amount){
         System.out.println(amount + " Deposited to your Savings Account.!");
     }
 
@@ -19,7 +21,7 @@ public class BankAppWithdrawalService {
         System.out.println(amount + " Withdrawn from your Current Account.!");
     }
 
-    /* I want to implement another feature for Banking App Withdrawal Service*/
+    *//* I want to implement another feature for Banking App Withdrawal Service*//*
 
     public void depositToSalarySaverAccount(double amount){
         System.out.println(amount + " Deposited to your Salary Server Account.!");
@@ -29,12 +31,28 @@ public class BankAppWithdrawalService {
         System.out.println(amount + " Withdrawn from your Salary Server Account.!");
     }
 
-    /* So here for adding a new feature I had to modify the class */
-    /* So, unfortunately we have violated the Open / Close principle */
-    /* Why.? */
-    /* In Open/Close principle a class should be closed for modifications but opened for extensions */
-    /* But here we did modifications rather than doing extensions */
-    /* S0, we violate the rule */
+    *//* So here for adding a new feature I had to modify the class *//*
+     *//* So, unfortunately we have violated the Open / Close principle *//*
+     *//* Why.? *//*
+     *//* In Open/Close principle a class should be closed for modifications but opened for extensions *//*
+     *//* But here we did modifications rather than doing extensions *//*
+     *//* S0, we violate the rule */
 
+    Account account;
 
+    public BankAppWithdrawalService(Account account) {
+        this.account = account;
+    }
+
+    public void depositToAccount(double amount){
+        account.deposit(amount);
+    }
+    public void withdrawFromAccount(double amount){
+        account.withdraw(amount);
+    }
+
+    /* Now what..? */
+    /* Now if there were thousands of account types in the future, no matter */
+    /* It can be used through the Account Class */
+    /* Then this class should not be modified (Closed for modifications), but this class is open for extensions */
 }
