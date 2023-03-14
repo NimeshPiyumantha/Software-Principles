1458c666bbaa718a8259b6f11bdc6fce5d045ed3
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-15 04:20:33 +0530

    update BankAppWithdrawalService

--- _01_Open-Close-Principle/src/BankAppWithdrawalService.java
+++ _01_Open-Close-Principle/src/BankAppWithdrawalService.java
@@ -18,4 +18,23 @@ public class BankAppWithdrawalService {
     public void withdrawFromCurrentAccount(double amount){
         System.out.println(amount + " Withdrawn from your Current Account.!");
     }
+
+    /* I want to implement another feature for Banking App Withdrawal Service*/
+
+    public void depositToSalarySaverAccount(double amount){
+        System.out.println(amount + " Deposited to your Salary Server Account.!");
+    }
+
+    public void withdrawFromSalarySaverAccount(double amount){
+        System.out.println(amount + " Withdrawn from your Salary Server Account.!");
+    }
+
+    /* So here for adding a new feature I had to modify the class */
+    /* So, unfortunately we have violated the Open / Close principle */
+    /* Why.? */
+    /* In Open/Close principle a class should be closed for modifications but opened for extensions */
+    /* But here we did modifications rather than doing extensions */
+    /* S0, we violate the rule */
+
+
 }
