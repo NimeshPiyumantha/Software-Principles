f1607a2e819daafddba08d1080ce6ad502c07966
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-15 04:59:36 +0530

    Update main class

---
+++ _02_Liscov-Substitution-Principle/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
