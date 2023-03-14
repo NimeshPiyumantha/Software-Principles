33df784891bb3b5b67489272f96d7e4641a377f0
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-15 04:30:55 +0530

    Update main class

--- _01_Open-Close-Principle/src/Main.java
+++ _01_Open-Close-Principle/src/Main.java
@@ -1,3 +1,9 @@
+import service.Account;
+import service.types.AnagiAccount;
+import service.types.CurrentAccount;
+import service.types.SalarySaverAccount;
+import service.types.SavingAccount;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
@@ -11,12 +17,40 @@ public class Main {
      * */
     public static void main(String[] args) {
 
-        BankAppWithdrawalService bankAppWithdrawalService = new BankAppWithdrawalService();
+        /*BankAppWithdrawalService bankAppWithdrawalService = new BankAppWithdrawalService();
 
         bankAppWithdrawalService.depositToCurrentAccount(100000);
         bankAppWithdrawalService.withdrawFromCurrentAccount(5000);
 
         bankAppWithdrawalService.depositToSalarySaverAccount(200000);
-        bankAppWithdrawalService.withdrawFromSalarySaverAccount(100000);
+        bankAppWithdrawalService.withdrawFromSalarySaverAccount(100000);*/
+
+
+
+        /* Let's deposit for Savings Account Bank App Withdrawal Service */
+        Account savingAccount= new SavingAccount();
+        BankAppWithdrawalService bankAppWithdrawalService1 = new BankAppWithdrawalService(savingAccount);
+        bankAppWithdrawalService1.depositToAccount(100000);
+        bankAppWithdrawalService1.depositToAccount(40000);
+
+
+        Account currentAccount= new CurrentAccount();
+        BankAppWithdrawalService bankAppWithdrawalService2 = new BankAppWithdrawalService(currentAccount);
+        bankAppWithdrawalService2.depositToAccount(200000);
+        bankAppWithdrawalService2.depositToAccount(30000);
+
+
+        Account salaryServer= new SalarySaverAccount();
+        BankAppWithdrawalService bankAppWithdrawalService3 = new BankAppWithdrawalService(salaryServer);
+        bankAppWithdrawalService3.depositToAccount(500000);
+        bankAppWithdrawalService3.depositToAccount(100000);
+
+        /* This is what we called as Open / Close Principle */
+
+        Account anagiAccount= new AnagiAccount();
+        BankAppWithdrawalService bankAppWithdrawalService4= new BankAppWithdrawalService(anagiAccount);
+        bankAppWithdrawalService4.withdrawFromAccount(100);
+        bankAppWithdrawalService4.depositToAccount(2000);
+
     }
 }
