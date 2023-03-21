0fb85be1ef1e85325cd0a4404550a79e3e9eab8c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 18:59:56 +0530

    Update Content

--- _04_Dependancy-Inversion-Principle/src/Main.java
+++ _04_Dependancy-Inversion-Principle/src/Main.java
@@ -1,9 +1,22 @@
+import service.Boy;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class Main {
+    /* SOLID */
+    /* Dependency Inversion Principle */
+    /*
+     * In this principle, it states that high level modules should not depend upon low level modules
+     * Both should be depended upon abstraction.
+     * This help us to develop loosely coupled code by ensuring that high-level modules depends on
+     * abstraction rather than concrete implementations of lower-level modules.
+     * The dependency injection pattern is an implementation of this principle.
+     * */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Boy boy= new Boy(); //Boy is a High Level Class
+        boy.chatWithGirl();
     }
 }
